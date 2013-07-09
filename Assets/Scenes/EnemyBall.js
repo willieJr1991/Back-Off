2a8054cf948d028ce52bda2f9b40bb0d23c9e121
Willie Johnson Jr <williejr1991@gmail.com>
2013-07-08 23:06:49 -0500

    Modified the Enemy Ball's gravity

--- Assets/Scenes/EnemyBall.js
+++ Assets/Scenes/EnemyBall.js
@@ -7,24 +7,20 @@ function Start ()
 {
 	gravitation_point = Hill.position;
 	gravitation_point.x += transform.position.x;
-	
-	
 }
 
 function Update () 
 {
+	//have the enemy move in it's current forward direction
 	transform.rigidbody.AddForce(transform.forward);
-	var force : Vector3 = gravitation_point - transform.position;
-	transform.rigidbody.AddForce(force);
 	
-	transform.forward =  Vector3.Cross(force, transform.right).normalized;
+	//get the "gravity force" from the hill and pull the enemy towards the center
+	var force : Vector3 = gravitation_point - transform.position;
+	transform.rigidbody.AddForce(force*1.5);
+	
+	//then make sure that it's up vector is pointing towards the center
+	transform.forward = Vector3.Cross(transform.right, force).normalized;
+	transform.up = -force;
+	//to make it travel up the hill in the right direction, flip the Y angle
+	transform.Rotate(180,0,0);
 }
-
-function OnCollisionStay(object : Collision)
-{
-	if (object.transform.tag == "Hill")
-	{
-		
-	}
-}
-
